1fa19183ba85326103667209a910dcc3a58bf5a2
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-01-02 10:28:46 -0800

    Unknown

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,39 +1,40 @@
 
-    var app = angular.module("mboutiqueApp", ['ngRoute']);
+var app = angular.module('app', ['ngRoute']);
 
 app.config(function($routeProvider){
 
-    $routeProvider
-        .when('/', {
-            templateUrl: "home.html",
-            controller: "mainController"
-        })
-        .when('/our-macarons', {
-            templateUrl: "macarons.html",
-            controller: "macaronsController"
-        })
-        .when('/gifts-parties', {
-            templateUrl: "gifts-parties.html",
-            controller: "giftsController"
-        })
-        .when('/contact', {
-            templateUrl: "contact.html",
-            controller: "contactController"
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+    $routeProvider.when('/', {
+        templateUrl: 'home.html',
+        controller: 'homeController',
+        controllerAs: 'hc'
+    }).when('/contact-us', {
+        templateUrl: 'contact.html',
+        controller: 'contactController',
+        controllerAs: 'cc'
+    }).when('/gifts-parties', {
+        templateUrl: 'gifts-parties.html',
+        controller: 'giftController',
+        controllerAs: 'gc'
+    }).when('/our-macarons', {
+        templateUrl: 'macarons.html',
+        controller: 'macaronsController',
+        controllerAs: 'mc'
+    }).otherwise({
+        redirectTo: '/'
+    })
 });
 
-app.controller('mainController', function($scope){
-    $scope.banner = "Assets/images/welcome-image.png";
+app.controller('homeController', function(){
+    this.imgUrl = 'Assets/images/welcome-image.png';
 
-}).controller('macaronsController', function($scope){
-    $scope.banner = "Assets/images/our-macarons-image.png";
 
-}).controller('giftsController', function($scope){
-    $scope.banner = "Assets/images/gifts-parties-image.png";
+}).controller('contactController', function(){
+    this.imgUrl = 'Assets/images/contact-image.png';
 
-}).controller('contactController', function($scope){
-    $scope.banner = "Assets/images/contact-image.png";
+}).controller('giftController', function(){
+    this.imgUrl = 'Assets/images/gifts-parties-image.png';
+
+}).controller('macaronsController', function(){
+    this.imgUrl = 'Assets/images/our-macarons-image.png';
+
 });
