9604c32057cb85622b45a0049a6afaa3cc8d0235
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-01-02 12:14:29 -0800

    Removed the use of scope

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,32 +1,31 @@
 
 var app = angular.module('app', ['ngRoute']);
 
-app.config(function($routeProvider){
+    app.config(function($routeProvider){
 
-    $routeProvider.when('/', {
-        templateUrl: 'home.html',
-        controller: 'homeController',
-        controllerAs: 'hc'
-    }).when('/contact-us', {
-        templateUrl: 'contact.html',
-        controller: 'contactController',
-        controllerAs: 'cc'
-    }).when('/gifts-parties', {
-        templateUrl: 'gifts-parties.html',
-        controller: 'giftController',
-        controllerAs: 'gc'
-    }).when('/our-macarons', {
-        templateUrl: 'macarons.html',
-        controller: 'macaronsController',
-        controllerAs: 'mc'
-    }).otherwise({
-        redirectTo: '/'
-    })
-});
+        $routeProvider.when('/', {
+            templateUrl: 'home.html',
+            controller: 'homeController',
+            controllerAs: 'hc'
+        }).when('/contact-us', {
+            templateUrl: 'contact.html',
+            controller: 'contactController',
+            controllerAs: 'cc'
+        }).when('/gifts-parties', {
+            templateUrl: 'gifts-parties.html',
+            controller: 'giftController',
+            controllerAs: 'gc'
+        }).when('/our-macarons', {
+            templateUrl: 'macarons.html',
+            controller: 'macaronsController',
+            controllerAs: 'mc'
+        }).otherwise({
+            redirectTo: '/'
+        })
+    });
 
 app.controller('homeController', function(){
     this.imgUrl = 'Assets/images/welcome-image.png';
-
 
 }).controller('contactController', function(){
     this.imgUrl = 'Assets/images/contact-image.png';
